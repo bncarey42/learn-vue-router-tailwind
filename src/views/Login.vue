c19b8c1118360f789ce7bcc8677d75c4468a2782
Ben Carey <bncarey42@gmail.com>
2020-06-27 23:42:40 -0500

    update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,43 +1,49 @@
 <template>
   <section>
-    <nav-bar :rootLink='rootLink' />
+    <nav-bar :rootLink="rootLink" />
     <div class="container items-center content-center text-gray-800">
-        <div class="w-full max-w-3xl m-auto mt-25">
-          <div class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 border border-green-900 border-opacity-25">
-            <div class="mb-4">
-              <label class="block text-gray-700 text-sm font-sans text-xl font-bold mb-2" 
-                     for="username">
-                Username
-              </label>
-              <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-                     id="username" 
-                     type="text" 
-                     placeholder="Username">
+      <div class="w-full max-w-2xl m-auto mt-32">
+        <div
+          class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4 border border-green-900 border-opacity-25"
+        >
+          <div class="mb-4">
+            <div class="border rounded w-full py-3 px-3 bg-white flex">
+              <input
+                class="appearance-none text-gray-700 leading-tight focus:outline-none flex-grow"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+              <!-- <router-link to="/forgot" class="m-0 rounded-sm border border-gray-400 text-gray-400 hover:border-red-400 hover:text-red-400 px-2 py-1">I Forgot</router-link> -->
             </div>
-            <div class="mb-6">
-              <label class="block text-gray-700 text-sm font-sans text-xl font-bold mb-2" 
-                     for="password">
-                Password
-              </label>
-              <input :class="`appearance-none border ${passwordError ? 'border-red-500':'' } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none`" 
-                     id="password" 
-                     type="password" 
-                     placeholder="Password">
-              <p class="text-red-500 text-xs italic" v-if="passwordError">{{ passwordErrorMsg }}</p>
+          </div>
+          <div class="mb-4">
+            <div
+              :class="`border ${passwordError ? 'border-red-500':'' } rounded w-full py-2 px-3 bg-white flex`"
+            >
+              <input
+                class="appearance-none text-gray-700 leading-tight focus:outline-none flex-grow"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+              <router-link
+                to="/forgot"
+                class="m-0 rounded-sm border border-gray-400 text-gray-400 hover:border-red-400 hover:text-red-400 px-2 py-1"
+              >I Forgot</router-link>
             </div>
-            <div class="flex items-center justify-between">
-              <router-link class="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
-                      @click="console.log('login')"
-                      to="/secure">
-                Sign In
-              </router-link>
-              <a class="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-gray-600" href="#" @click.stop.prevent="forgot = true">
-               Forgot Something?
-              </a>
-            </div>
+            <p class="text-red-500 text-xs italic" v-if="passwordError">{{ passwordErrorMsg }}</p>
+          </div>
+          <div class="flex items-center justify-between">
+            <router-link
+              class="bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+              @click="console.log('login')"
+              to="/secure"
+            >Sign In</router-link>
           </div>
         </div>
       </div>
+    </div>
   </section>
 </template>
 
@@ -46,15 +52,12 @@ export default {
   name: "Login",
   data: function() {
     return {
-      rootLink: {link: '/', 
-      name:'Owlsight'},
+      rootLink: { link: "/", name: "Owlsight" },
       passwordError: false,
-      passwordErrorMsg: 'Please choose a password.',
+      passwordErrorMsg: "Please choose a password.",
       forgot: false
-    }
+    };
   },
-  methods: {
-    
-  }
-}
+  methods: {}
+};
 </script>
